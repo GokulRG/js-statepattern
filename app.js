96ca86aa603ca7efde6a4867e70636f94ed49900
Gokul Ramakrishnan <ramakrishnan.go@husky.neu.edu>
2020-01-15 06:49:06 -0800

    First Commit
    Adding index.html and app.js to demonstrate state pattern

---
+++ app.js
@@ -0,0 +1,79 @@
+//State Pattern
+const pageState = function () {
+    let currentState;
+
+    this.init = function () {
+        this.change(new homeState);
+    }
+
+    this.change = function (state) {
+        currentState = state;
+    }
+};
+
+//Home State
+const homeState = function () {
+    document.querySelector('#heading').textContent = null;
+    document.querySelector('#content').innerHTML = `
+        <div class="jumbotron">
+            <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    `;
+}
+
+//About state
+const aboutState = function () {
+    document.querySelector('#heading').textContent = 'About Us';
+    document.querySelector('#content').innerHTML = `
+        <p> This is the About page</p>
+    `;
+};
+
+//Contact state
+const contactState = function () {
+    document.querySelector('#heading').textContent = 'Contact Us';
+    document.querySelector('#content').innerHTML = `
+     <form>
+        <div class="form-group">
+            <label>Name</label>
+            <input type="text" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Email Address</label>
+            <input type="email" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    `;
+};
+
+//Instantiate the page state
+const page = new pageState();
+
+//Init the first page
+page.init();
+
+//UI Variables
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const contact = document.getElementById('contact');
+
+//Home
+home.addEventListener('click', (ev => {
+    ev.preventDefault();
+    page.change(new homeState);
+}));
+//About
+about.addEventListener('click', (ev => {
+    ev.preventDefault();
+    page.change(new aboutState);
+}));
+//Contact
+contact.addEventListener('click', (ev => {
+    ev.preventDefault();
+    page.change(new contactState);
+}));
